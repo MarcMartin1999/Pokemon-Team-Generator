223e83342a5cd8ae718f3785921657aab9951b46
MarcMartin1999 <martinmarc1999@gmail.com>
2020-04-21 13:29:10 +0200

    Update

--- Scripts/PokemonFinder.py
+++ Scripts/PokemonFinder.py
@@ -1,42 +1,48 @@
 from selenium.webdriver import Firefox
-from selenium import webdriver
 from  Pokemon import PokemonObject
-from Types import createTableTypes,assingPokemon
+from Types import createTableTypes , assingPokemon, pokemonsOrderByTypes
+from selenium import webdriver as webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+ 
 
-driver = Firefox()
+binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
+driver = webdriver.Firefox(firefox_binary=binary,executable_path=r"C:\\geckodriver.exe")
 
-
-
-with Firefox() as driver:
+#CODE   
+driver.get("https://pokemondb.net/pokedex/national") 
+pokemonsName =driver.find_elements_by_class_name("ent-name")
+pokemonsTypeContainer =driver.find_elements_by_class_name("infocard")  
    
-   driver.get("https://pokemondb.net/pokedex/national") 
-   pokemonsName =driver.find_elements_by_class_name("ent-name")
-   pokemonsTypeContainer =driver.find_elements_by_class_name("infocard")  
-   
-   for x in range(len(pokemonsName)):
+for x in range(len(pokemonsName)):
       
      
-      pokemonAuxType = pokemonsTypeContainer[x].find_elements_by_css_selector("small>a")
+   pokemonAuxType = pokemonsTypeContainer[x].find_elements_by_css_selector("small>a")
       
-      if len(pokemonAuxType)  == 2:
+   if len(pokemonAuxType)  == 2:
 
-         type1= pokemonAuxType[1].text
-         type2= pokemonAuxType[0].text
-         name = pokemonsName[x].text
+      type1= pokemonAuxType[1].text
+      type2= pokemonAuxType[0].text
+      name = pokemonsName[x].text
 
-         pokemonAux = PokemonObject(name,type1,type2)
-         assingPokemon(pokemonAux)
+      pokemonAux = PokemonObject(name,type1,type2)
+      assingPokemon(pokemonAux)
 
-      if len(pokemonAuxType)  == 1:
+   if len(pokemonAuxType)  == 1:
 
-         type1= pokemonAuxType[0].text
-         name = pokemonsName[x].text
+      type1= pokemonAuxType[0].text
+      name = pokemonsName[x].text
 
-         pokemonAux = PokemonObject(name,type1)
-         assingPokemon(pokemonAux)
+      pokemonAux = PokemonObject(name,type1)
+      assingPokemon(pokemonAux)
 
          
-   driver.quit
-
+driver.quit
 createTableTypes()
+print(pokemonsOrderByTypes[0][1])
+print(pokemonsOrderByTypes[0])
+print(pokemonsOrderByTypes[1][3])
+print(pokemonsOrderByTypes[1])
+print(pokemonsOrderByTypes[4][1])
+print(pokemonsOrderByTypes[4])
+
 
--- Scripts/Types.py
+++ Scripts/Types.py
@@ -1,15 +1,21 @@
 from Pokemon import PokemonObject
 from selenium.webdriver import Firefox
-from selenium import webdriver
+from selenium import webdriver 
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
 
-driver = Firefox()
+binary = FirefoxBinary("C:\\Program Files\\Mozilla Firefox\\firefox.exe")
+driver = webdriver.Firefox(firefox_binary=binary,executable_path=r"C:\\geckodriver.exe")
+
+
+
+
+#CODE
 pokemonsOrderByTypes = [[],[]]
 
 
 def createTableTypes():
 
     with Firefox() as driver:
-
 
         driver.get("https://pokemondb.net/type") 
         pokemonsType = driver.find_elements_by_css_selector("p.text-center>a")
@@ -23,6 +29,6 @@ def assingPokemon(pok):
     if type(pok) == PokemonObject:
         i = 0
         for x in pokemonTypes[0]:
-            if pok.pokemonType1 == x or pok.pokemonType2 == x:
+            if pok.pokemonsOrderByTypes == x or pok.pokemonType2 == x:
                     PokemonTypes[i][i]=pok
             i=i+1
